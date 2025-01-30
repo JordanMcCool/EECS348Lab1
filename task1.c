cd7ac7bf54219afbcd37ed86695a99a1ee57d008
JordanMcCool <jordan.mccool@ku.edu>
2025-01-30 09:10:12 -0600

    Add hello-world program
    task 1

---
+++ task1.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main() // didnt write this. www.onlinegdb.com by default is a hello world
+{
+    printf("hello world");
+
+    return 0;
+}
