f5cb91e994f499e92afe59b290785bfa9cf2733a
JordanMcCool <jordan.mccool@ku.edu>
2025-01-30 09:25:08 -0600

    Replace the hello-world program with the sample C program.
    actual step 3, kept misunderstanding instructions

--- task1.c
+++ task1.c
@@ -1,16 +1,29 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
+#include <string.h>
+int main() {
+	char str[100];
+	int i, len;
 
-int main() // actual step 3
-{
-    printf("bye world");
+	printf("Enter a string: ");
+	gets(str);
 
-    return 0;
+	len = strlen(str);
+	for (i = 0; i < len; i++) {
+		if (str[i] >= 'a' && str[i] <= 'z') {
+		str[i] = str[i] - 'a' + 'A';
+	}
+}
+
+printf("The capitalized string is: %s\n", str);
+
+// Reverse the string.
+for (i = 0, len = strlen(str); i < len / 2; i++) {
+	char temp = str[i];
+	str[i] = str[len - 1 - i];
+	str[len - 1 - i] = temp;
+}
+
+printf("The reversed string is: %s\n", str);
+
+return 0;
 }
